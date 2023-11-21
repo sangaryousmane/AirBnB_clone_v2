01b3f54ebc7a84bb3259ac08506af099d54b94fe
Hamilton Sebastine <hamilsebastine@gmail.com>
2023-11-21 12:33:30 +0000

    Implement Optional Filtering

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -8,8 +8,15 @@ class FileStorage:
     __file_path = 'file.json'
     __objects = {}
 
-    def all(self):
+    def all(self, cls=None):
         """Returns a dictionary of models currently in storage"""
+        if cls is not None:
+            cls_dict = {}
+            for key, inst in FileStorage.__objects.items():
+                cls_name = (str(type(inst)).split('.')[-1]).split('\'')[0]
+                if type(inst) == cls:
+                    cls_dict[key] =  inst
+            return cls_dict
         return FileStorage.__objects
 
     def new(self, obj):
@@ -48,3 +55,8 @@ class FileStorage:
                         self.all()[key] = classes[val['__class__']](**val)
         except FileNotFoundError:
             pass
+
+    def delete(self, obj=None):
+        """Delete object from the dictionary"""
+        if obj is not None:
+            self.all().pop(obj.to_dict()['__class__'] + '.' + obj.id)
