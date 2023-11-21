e6e049609075fab2f64bdb945616d0ee3f20c3f8
Hamilton Sebastine <hamilsebastine@gmail.com>
2023-11-21 07:54:39 +0000

    Update setup_mysql_test.sql

--- setup_mysql_test.sql
+++ setup_mysql_test.sql
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
--- Write a script that prepares a MySQL test server for the project
+-- This script prepares a MySQL server for the project:
 
 CREATE DATABASE IF NOT EXISTS hbnb_test_db;
-CREATE USER IF NOT EXISTS 'hbnb_test'@'localhost' IDENTIFIED BY 'hbnb_test_pwd';
-GRANT ALL PRIVILIGES ON 'hbnb_test_db' TO 'hbnb_test'@'localhost';
-GRANT SELECT ON 'performance_schema' TO 'hbnb_test'@'localhost';
+CREATE USER IF NOT EXISTS 'hbnb_test'@'localhost' IDENTIFIED BY 'hbnb_dev_pwd';
+GRANT ALL PRIVILEGES ON hbnb_test_db.* TO 'hbnb_test'@'localhost';
+GRANT SELECT ON performance_schema.* TO 'hbnb_test'@'localhost';
