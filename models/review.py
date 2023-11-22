5428dabb0b4d961463a02aca374df623da0c82e8
sangaryousmane <sangaryousmane@gmail.com>
2023-11-22 07:12:16 -0800

    Validate user

--- models/review.py
+++ models/review.py
@@ -1,11 +1,17 @@
 #!/usr/bin/python3
 """ Review module for the HBNB project """
 from models.base_model import BaseModel, Base
+from sqlalchemy import Column, String, ForeignKey
+import sqlalchemy
 
 
-class Review(BaseModel):
+class Review(BaseModel, base):
     """ Review classto store review information """
     __tablename__ = "reviews"
     text = Column(String(1024), nullable=False)
     place_id = Column(String(60), nullable=False, ForeignKey("places.id"))
     user_id = Column(String(60), nullable=False, ForeignKey("users.id"))
+
+    def __init__(self, *args, **kwargs):
+        """ Initialize the Review model"""
+        super().__init__(*args, **kwargs);
--- models/user.py
+++ models/user.py
@@ -4,17 +4,26 @@ from models.base_model import BaseModel, Base
 from sqlalchemy import Column, String
 from sqlalchemy.orm import relationship
 import models
+from os import getenv
+import sqlalchemy
 
 
 class User(BaseModel, Base):
     """This class defines a user by various attributes"""
+
+    if models.storage_type == "db":
         __tablename__ = "users"
         email = Column(String(128), nullable=False)
         password = Column(String(128), nullable=False)
-        first_name = Column(String(128), nullable=False)
-        last_name = Column(String(128), nullable=False)
+        first_name = Column(String(128), nullable=True)
+        last_name = Column(String(128), nullable=True)
         places = relationship("Place", backref="user")
         reviews = relationship("Review", backref="user")
+    else:
+        email = ""
+        password = ""
+        first_name = ""
+        last_name = ""
 
     def __init__(self, *args, **kwargs):
         """ Initialize the user model"""
