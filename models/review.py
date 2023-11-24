10601a7b12d66a9571ef4fb35a156635b6e7ab95
sangaryousmane <sangaryousmane@gmail.com>
2023-11-23 17:00:00 -0800

    Update file storage

--- models/review.py
+++ models/review.py
@@ -5,12 +5,12 @@ from sqlalchemy import Column, String, ForeignKey
 import sqlalchemy
 
 
-class Review(BaseModel, base):
+class Review(BaseModel, Base):
     """ Review classto store review information """
     __tablename__ = "reviews"
     text = Column(String(1024), nullable=False)
-    place_id = Column(String(60), nullable=False, ForeignKey("places.id"))
-    user_id = Column(String(60), nullable=False, ForeignKey("users.id"))
+    place_id = Column(String(60), ForeignKey("places.id"), nullable=False)
+    user_id = Column(String(60), ForeignKey("users.id"), nullable=False)
 
     def __init__(self, *args, **kwargs):
         """ Initialize the Review model"""
