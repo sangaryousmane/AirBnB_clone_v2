0839fd47ce78bca4942d862929e5f7cda961d6ab
sangaryousmane <sangaryousmane@gmail.com>
2023-12-24 07:35:14 -0800

    Modify

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -1,8 +1,10 @@
 #!/usr/bin/python3
+"""Using flask framework
+"""
 
 from flask import Flask
-
 app = Flask(__name__)
+
 
 @app.route("/", strict_slashes=False)
 def hello():
@@ -11,4 +13,4 @@ def hello():
 
 # Run main application
 if __name__=='__main__':
-    app.run(host='0.0.0.0', port=5000)
+    app.run()
